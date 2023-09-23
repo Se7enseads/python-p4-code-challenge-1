ba85be107863bd77080611fcdbe6abd358fd898b
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-09-23 11:32:40 +0300

    feat: add docstring to `models.py`

--- server/models.py
+++ server/models.py
@@ -1,9 +1,29 @@
+"""
+Database Models:
+    The application uses the following SQLAlchemy database models:
+
+    - `Restaurant`: Represents restaurant information, including name and address.
+    - `Pizza`: Represents pizza information, including name and ingredients.
+    - `RestaurantPizza`: Represents the association between restaurants and pizzas,
+                         including the price.
+"""
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
+    """
+    Database model representing a restaurant.
+
+    Attributes:
+        - id (int): The primary key for the restaurant.
+        - name (str): The name of the restaurant (unique).
+        - address (str): The address of the restaurant.
+
+        - restaurant_pizzas (Relationship): One-to-many relationship with RestaurantPizza.
+    """
 
     __tablename__ = 'restaurants'
 
@@ -15,6 +35,17 @@ class Restaurant(db.Model):
 
     @validates('name')
     def name_validation(self, key, name):
+        """
+        Validate the name of the restaurant.
+
+        Args:
+            key (str): The name of the attribute being validated ('name').
+            name (str): The name value to be validated.
+
+        Raises:
+            ValueError: If the name is not unique or exceeds 50 characters in length.
+        """
+
         names = db.session.query(Restaurant.name).all()
 
         if name in names:
@@ -29,6 +60,18 @@ class Restaurant(db.Model):
         return f'<Restaurant: Name: {self.name} Address: {self.address}>'
 
 class Pizza(db.Model):
+    """
+    Database model representing a pizza.
+
+    Attributes:
+        - id (int): The primary key for the pizza.
+        - name (str): The name of the pizza.
+        - ingredients (str): The ingredients of the pizza.
+        - created_at (DateTime): The creation timestamp for the pizza.
+        - updated_at (DateTime): The update timestamp for the pizza.
+        
+        - restaurant_pizzas (Relationship): One-to-many relationship with RestaurantPizza.
+    """
 
     __tablename__ = 'pizzas'
 
@@ -44,6 +87,20 @@ class Pizza(db.Model):
         return f'<Pizza: Name: {self.name} Ingredients: {self.ingredients}>'
 
 class RestaurantPizza(db.Model):
+    """
+    Database model representing a restaurant-pizza association.
+
+    Attributes:
+        - id (int): The primary key for the restaurant-pizza association.
+        - pizza_id (int): The foreign key reference to the pizza.
+        - restaurant_id (int): The foreign key reference to the restaurant.
+        - price (int): The price of the pizza at the restaurant.
+        - created_at (DateTime): The creation timestamp for the association.
+        - updated_at (DateTime): The update timestamp for the association.
+        
+        - pizza (Relationship): Many-to-one relationship with Pizza.
+        - restaurant (Relationship): Many-to-one relationship with Restaurant.
+    """
 
     __tablename__ = 'restaurant_pizzas'
 
@@ -59,6 +116,17 @@ class RestaurantPizza(db.Model):
 
     @validates('price')
     def price_validation(self, key, price):
+        """
+        Validate the price of the restaurant pizza.
+
+        Args:
+            key (str): The name of the attribute being validated ('price').
+            price (int): The price value to be validated.
+
+        Raises:
+            ValueError: If the price is not within the range [1, 30].
+        """
+
         if price not in range(1, 31):
             raise ValueError('Price must be between 1 and 30')
         return price
